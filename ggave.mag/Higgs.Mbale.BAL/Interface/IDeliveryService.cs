b610e5baf120536afff6402a53c3dfcb2c252d58
Denicos <nicoswaves.mn@gmail.com>
2021-06-13 20:29:54 +0300

    initiall push
    .....

---
+++ ggave.mag/Higgs.Mbale.BAL/Interface/IDeliveryService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Higgs.Mbale.Models;
+
+namespace Higgs.Mbale.BAL.Interface
+{
+    public interface IDeliveryService
+    {
+        IEnumerable<Delivery> GetAllDeliveries();
+        Delivery GetDelivery(long deliveryId);
+        long SaveDelivery(Delivery delivery, string userId);
+        void MarkAsDeleted(long deliveryId, string userId);
+        IEnumerable<Delivery> GetAllDeliveriesForAParticularBranch(long branchId);
+        IEnumerable<Delivery> GetAllDeliveriesForAParticularOrder(long orderId);
+        IEnumerable<Delivery> MapEFToModel(IEnumerable<EF.Models.Delivery> data);
+        IEnumerable<Delivery> GetAllUnApprovedDeliveries();
+        IEnumerable<Delivery> GetAllRejectedDeliveries();
+
+        IEnumerable<Delivery> GetAllApprovedDeliveries();
+
+        IEnumerable<Delivery> GetAllBranchUnApprovedDeliveries(long branchId);
+        IEnumerable<Delivery> GetAllBranchRejectedDeliveries(long branchId);
+        IEnumerable<Delivery> GetAllBranchApprovedDeliveries(long branchId);
+
+    }
+}
